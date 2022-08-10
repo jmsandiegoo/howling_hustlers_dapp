1148e9c203e5bb3f6a71c4156091c702f05923d8
Jm San Diego <jmsandiego0710@gmail.com>
2022-08-10 20:09:38 +0800

    Team section added

--- pages/home.tsx
+++ pages/home.tsx
@@ -43,9 +43,11 @@ const HomePage: NextPage = () => {
         </section>
         <section className={`container ${styles["homepage__overview"]}`}>
           <div className="content">
-            <h2>PROJECT OVERVIEW</h2>
+            <h2 className={`${styles["overview__heading"]}`}>
+              PROJECT OVERVIEW
+            </h2>
             <p>
-              Howling Hustlers is a wolf/huster inspired NFT collection that
+              Howling Hustlers is a wolf/hustler inspired NFT collection that
               consists of different traits. Our goal is to simply release cool
               looking art to our holders for them to collect and showcase.
             </p>
@@ -57,6 +59,70 @@ const HomePage: NextPage = () => {
               and Public mint prices are 100 and 200 MATIC respectively
             </p>
           </div>
+        </section>
+        <section className={`container ${styles["homepage__team"]}`}>
+          <div className="content">
+            <h2 className={`${styles["team__heading"]}`}>
+              <span>MEET</span>
+              <br />
+              <span>THE WOLF TEAM</span>
+            </h2>
+            <figure className={`${styles["team__member"]}`}>
+              <Image
+                className={`${styles["member__image"]}`}
+                src={`/images/Landing_NFT1.jpg`}
+                alt="Howling Hustlers Landing Banner"
+                width={200}
+                height={200}
+              />
+              <figcaption>
+                <h3 className={`${styles["member__name"]}`}>JM SAN DIEGO</h3>
+                <p className={`${styles["member__role"]}`}>Developer</p>
+              </figcaption>
+            </figure>
+            <figure className={`${styles["team__member"]}`}>
+              <Image
+                className={`${styles["member__image"]}`}
+                src={`/images/Landing_NFT2.jpg`}
+                alt="Howling Hustlers Landing Banner"
+                width={200}
+                height={200}
+              />
+              <figcaption>
+                <h3 className={`${styles["member__name"]}`}>BRENDAN ANG</h3>
+                <p className={`${styles["member__role"]}`}>WEB3.0 Expert</p>
+              </figcaption>
+            </figure>
+            <figure className={`${styles["team__member"]}`}>
+              <Image
+                className={`${styles["member__image"]}`}
+                src={`/images/Landing_NFT4.jpg`}
+                alt="Howling Hustlers Landing Banner"
+                width={200}
+                height={200}
+              />
+              <figcaption>
+                <h3 className={`${styles["member__name"]}`}>YILIN</h3>
+                <p className={`${styles["member__role"]}`}>Artist</p>
+              </figcaption>
+            </figure>
+            <figure className={`${styles["team__member"]}`}>
+              <Image
+                className={`${styles["member__image"]}`}
+                src={`/images/Landing_NFT3.jpg`}
+                alt="Howling Hustlers Landing Banner"
+                width={200}
+                height={200}
+              />
+              <figcaption>
+                <h3 className={`${styles["member__name"]}`}>SANDY KEE</h3>
+                <p className={`${styles["member__role"]}`}>UI/UX Designer</p>
+              </figcaption>
+            </figure>
+          </div>
+        </section>
+        <section className={`container ${styles["homepage__faq"]}`}>
+          <div className="content"></div>
         </section>
       </section>
     </MainPageLayout>
